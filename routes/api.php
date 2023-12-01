de0e9d052adaa08fead9cc6075e5c58e4a8acb6c
= <=>
2023-12-02 00:56:22 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -27,24 +27,24 @@ Route::prefix('/v1')->group(function () {
             Route::get('/staffs', [\App\Http\Controllers\Api\StaffController::class, 'index']);
             Route::post('/staffs', [\App\Http\Controllers\Api\StaffController::class, 'store']);
             Route::put('/staffs/{staff}', [\App\Http\Controllers\Api\StaffController::class, 'update']);
-            Route::delete('/staffs', [\App\Http\Controllers\Api\StaffController::class, 'destroy']);
+            Route::delete('/staffs/{staff}', [\App\Http\Controllers\Api\StaffController::class, 'destroy']);
             Route::get('/staffs/{staff}', [\App\Http\Controllers\Api\StaffController::class, 'show']);
 
             Route::get('/invoices', [\App\Http\Controllers\Api\InvoiceController::class, 'index']);
             Route::post('/invoices', [\App\Http\Controllers\Api\InvoiceController::class, 'store']);
             Route::put('/invoices/{invoice}', [\App\Http\Controllers\Api\InvoiceController::class, 'update']);
-            Route::delete('/invoices', [\App\Http\Controllers\Api\InvoiceController::class, 'destroy']);
+            Route::delete('/invoices/{invoice}', [\App\Http\Controllers\Api\InvoiceController::class, 'destroy']);
             Route::get('/invoices/{invoice}', [\App\Http\Controllers\Api\InvoiceController::class, 'show']);
 
             Route::get('/vouchers', [\App\Http\Controllers\Api\VoucherController::class, 'index']);
             Route::post('/vouchers', [\App\Http\Controllers\Api\VoucherController::class, 'store']);
             Route::put('/vouchers/{voucher}', [\App\Http\Controllers\Api\VoucherController::class, 'update']);
-            Route::delete('/vouchers', [\App\Http\Controllers\Api\VoucherController::class, 'destroy']);
+            Route::delete('/vouchers/{voucher}', [\App\Http\Controllers\Api\VoucherController::class, 'destroy']);
             Route::get('/vouchers/{voucher}', [\App\Http\Controllers\Api\VoucherController::class, 'show']);
 
             Route::get('/customers', [\App\Http\Controllers\Api\CustomerController::class, 'index']);
             Route::put('/customers/{customer}', [\App\Http\Controllers\Api\CustomerController::class, 'update']);
-            Route::delete('/customers', [\App\Http\Controllers\Api\CustomerController::class, 'destroy']);
+            Route::delete('/customers/{customer}', [\App\Http\Controllers\Api\CustomerController::class, 'destroy']);
             Route::get('/customers/{customer}', [\App\Http\Controllers\Api\CustomerController::class, 'show']);
 
             Route::post('/vouchers/verify', [\App\Http\Controllers\Api\VoucherVerifyController::class, '__invoke']);
